63928fb0f6d3fe258dbbed81d5aedf5d227fc1e0
Robert Edilber <robertedilber@gmail.com>
2021-10-08 18:49:16 -0400

    Added dialogue file scriptable template

---
+++ DialogSystem/DialogueFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DialogueFile : ScriptableObject, IEnumerable<GraphNodeData>
+{
+    private List<GraphNodeData> _nodeData = new List<GraphNodeData>();
+
+    public void AddNodeData(GraphNodeData graphNodeData)
+    {
+        _nodeData.Add(graphNodeData);
+    }
+
+    public void RemoveNodeData(GraphNodeData graphNodeData)
+    {
+        _nodeData.Remove(graphNodeData);
+    }
+
+    public void Clear()
+    {
+        _nodeData.Clear();
+    }
+
+    public IEnumerator<GraphNodeData> GetEnumerator()
+    {
+        return ((IEnumerable<GraphNodeData>)_nodeData).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)_nodeData).GetEnumerator();
+    }
+}
